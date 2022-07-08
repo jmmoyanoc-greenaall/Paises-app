f72a7a790b4916e165214fa0bd089d3adedf180d
David García <dgarcia@greenaall.es>
2022-07-08 09:15:56 +0200

    pais_08/07/22_2

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
+  styles: [
+    `
+    li{
+        cursor: Pointer;
+      }
+    `
+  ]
+  
 })
 export class PorPaisComponent  {
 
@@ -12,6 +21,8 @@ export class PorPaisComponent  {
   hayError:boolean=false;
   hayPais:boolean=false;
   paises:Country[] = [];
+  paisesSugeridos:Country[] = [];
+  mostrarSugerenicas:boolean = false;
 
   constructor(private paisService:PaisService) { }
 
@@ -37,7 +48,25 @@ export class PorPaisComponent  {
 
   sugerencias(termino:string){
     this.hayError=false;
-    
+    this.termino=termino;
+    this.mostrarSugerenicas=true;
+
+    let miObservadorS = {
+      next: (paises:Country[] ) => 
+      { console.log(paises);
+        this.paisesSugeridos=paises.splice(0,5)
+      },
+      error: (err: Error) => 
+      { this.paisesSugeridos = []
+      },
+      complete: () => console.log('Realizado')
+    }
+    this.paisService.buscarPais(termino).subscribe(miObservadorS);
+  }
+
+  buscarSugerido(termino:string){
+    this.buscar(termino);
+
   }
   
 }
